ac6d342371fc56dd4928df7879d4500d125028ab
joetodd109 <joetodd109@aol.com>
2015-06-25 20:23:34 +0100

    Now using magic tap rather than stepper motor concoction

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 #include "stepper.h"
 
 #define BUFFERSIZE      128u
-#define MOIST_LEVEL     3000u
+#define MOIST_LEVEL     2048u
 
 #define SENSOR_IN_PORT  iox_port_c
 #define SENSOR_IN_PIN   1u
@@ -32,21 +32,22 @@
 #define VALVE_ON_PIN    2u
 
 //#define TESTING         /* not testing mode */
-//#define VALVE           /* not using valve */
+#define VALVE             /* using valve */
 #define HOLD_TIME       86400u / 2u  /* one day - why are clock calculations out by 2? */
 
 /* Prototypes -----------------------------------------------------------------*/
 static uint16_t moisture[BUFFERSIZE] = {0};
 
-/* 
+/*
  * Turn on water flow for 'time' in ms.
+ * @note: stepper not used anymores
  */
 static void
 water_on(uint32_t time, uint16_t turns)
 {
     /* timer ticks at 1ms */
-    timer_reconfigure(0x001e, 0xFFFF); 
-    timer_delay(time); 
+    timer_reconfigure(0x001e, 0xFFFF);
+    timer_delay(time);
     iox_led_on(false, false, false, true);
     stepper_turn_cw(turns);
     timer_delay(time);
@@ -61,31 +62,31 @@ int main(void)
     iox_led_init();
     timer_init();
     adc_init();
-    stepper_init();
+    //stepper_init();
 
     uint32_t i = 0;
 
-    iox_configure_pin(SENSOR_IN_PORT, SENSOR_IN_PIN, iox_mode_analog, 
+    iox_configure_pin(SENSOR_IN_PORT, SENSOR_IN_PIN, iox_mode_analog,
             iox_type_pp, iox_speed_low, iox_pupd_none);
     iox_configure_pin(SENSOR_EN_PORT, SENSOR_EN_PIN, iox_mode_out,
             iox_type_pp, iox_speed_low, iox_pupd_down);
     iox_configure_pin(VALVE_ON_PORT, VALVE_ON_PIN, iox_mode_out,
             iox_type_pp, iox_speed_low, iox_pupd_down);
-    /* Test clock frequency 
+    /* Test clock frequency
     iox_configure_pin(iox_port_a, 8, iox_mode_af,
             iox_type_pp, iox_speed_high, iox_pupd_none);
     */
 
     while (1) {
         timer_reconfigure(0x7800, 0xFFFF);
-        iox_set_pin_state(SENSOR_EN_PORT, SENSOR_EN_PIN, true); 
+        iox_set_pin_state(SENSOR_EN_PORT, SENSOR_EN_PIN, true);
         timer_delay(2);
         moisture[i] = adc_get_measurement();
         timer_delay(1);
-        iox_set_pin_state(SENSOR_EN_PORT, SENSOR_EN_PIN, false);  
+        iox_set_pin_state(SENSOR_EN_PORT, SENSOR_EN_PIN, false);
 
         if (moisture[i] > MOIST_LEVEL) {
-            /* 
+            /*
              * Soil is too dry!
              */
 #ifdef VALVE
@@ -93,8 +94,8 @@ int main(void)
             timer_delay(5);
             iox_set_pin_state(VALVE_ON_PORT, VALVE_ON_PIN, false);
 #else
-            /* 
-             * Water flow on for 2.5s 
+            /*
+             * Water flow on for 2.5s
              */
             water_on(2500, 450);
 #endif
@@ -102,18 +103,18 @@ int main(void)
         i = (i + 1) % BUFFERSIZE;
 
 #ifdef TESTING
-        /* 
-         * Just wait 2secs if testing 
+        /*
+         * Just wait 2secs if testing
          */
         timer_reconfigure(0x7800, 0xFFFF);
         timer_delay(2);
 #else
-        /* 
+        /*
          * Else timer overflows in 24hours
          */
         timer_reconfigure(0xF000, HOLD_TIME);
-        /* 
-         * Sleep until timer overflows 
+        /*
+         * Sleep until timer overflows
          */
         __WFI();
 #endif
